941f6adfe11734e8a7fcbacb9a08b07468819c82
junzzan9 <ojs2681@gmail.com>
2021-06-17 17:10:31 +0900

    0617

---
+++ src/com/javaex/ex03/PersonApp.java
@@ -0,0 +1,74 @@
+package com.javaex.ex03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PersonApp {
+	public static void main(String[] args) throws IOException {
+
+		InputStream in = new FileInputStream("C:\\javaStudy\\file\\PhoneDB.txt");
+		InputStreamReader ir = new InputStreamReader(in, "UTF-8");
+		BufferedReader br = new BufferedReader(ir);
+		Writer fw = new FileWriter("C:\\javaStudy\\file\\PhoneDB01.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		List<Person> pList = new ArrayList<Person>();
+		Scanner sc = new Scanner(System.in);
+
+		String line = "";
+
+		while (true) {
+			line = br.readLine();
+			if (line == null) {
+				break;
+			}
+			String[] pInfo = line.split(",");
+			Person p = new Person(pInfo[0], pInfo[1], pInfo[2]);
+			pList.add(p);
+		}
+		ShowAll(pList);
+		br.close();
+
+
+		while (true) {
+			System.out.println("등록할 친구정보를 입력해주세요     종료:q");
+			String info = sc.next();
+			if (info.equals("q")) {
+				ShowAll(pList);
+				break;
+			}
+			System.out.println("친구정보를 몇번째로 저장하시겠습니까?");
+			int num = sc.nextInt();
+			String[] pInfo = info.split(",");
+			Person p = new Person(pInfo[0], pInfo[1], pInfo[2]);
+			pList.add(num,p);
+		}
+		ShowAll(pList);
+		
+		String str = "";
+		for (int i = 0; i < pList.size(); i++) {
+			str = pList.get(i).save();
+			bw.write(str);
+			bw.newLine();
+
+		}
+		bw.close();
+		
+		sc.close();
+	}
+
+	static void ShowAll(List<Person> arr) {
+		for (int i = 0; i < arr.size(); i++) {
+			arr.get(i).showInfo();
+		}
+	}
+
+}
